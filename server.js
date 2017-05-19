2ccec3b50fbcea5b4afe12cef6cfef2f008b8b5e
Sandi <s.milicevic@mail.utoronto.ca>
2017-05-18 22:32:13 -0400

    Installed ejs module Installed express module Created ejs template with canvas element Changed server to use express when handling requests and responses

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-var http = require('http');
-var server = http.createServer(); // create the web server object
+var fs = require('fs');
+var express = require('express');
+var app = express();
 
-server.on('request', function(request, response) {
-	response.write('<!DOCTYPE html>');
-	response.write('<html>');
-	response.write('<body>');
-	response.write('<canvas id="game" width="800" height="600" style="border:1px solid #000000;">');
-	response.write('Your browser does not support the HTML5 canvas tag.');
-	response.write('</canvas>');
-	response.write('</body>');
-	response.write('</html>');
-	response.end();
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+	res.render('index');
 });
 
-server.listen(8080);
-console.log('Server is running on port 8080...');
+app.listen(port, function() {
+    console.log('Server is running on port ' + port + '...');
+});
